f79b825dd94ee6feb320989d3f2370eb93aa9f3f
Tobby <sashapak97@mail.ru>
2019-04-18 16:14:36 +0300

    DOXYGEN

--- BookShop/urls.py
+++ BookShop/urls.py
@@ -4,12 +4,33 @@ from BookShop import views
 
 app_name = 'BookShop'
 urlpatterns = [
-    url(r'^register/$', views.RegisterFormView.as_view()), # Регистрация 
-    url(r'^login/$', views.LoginFormView.as_view()), # Аутентификация
+
+    ##Requests
+
+    ## Registration
+    # Parameters: name, email, password
+    # Method: POST
+    url(r'^register/$', views.RegisterFormView.as_view()),
+    ## Authentication
+    # Parameters: name/email, password
+    # Method: POST
+    url(r'^login/$', views.LoginFormView.as_view()),
     url(r'^logout/$', views.LogoutView.as_view()),
-    url(r'^$', TemplateView.as_view(template_name='home.html')), 
-    url(r'^product/$', views.product, name='product'), # Товары
-    url(r'^product/?select=max', views.product, name='product'), # Сортировка товаров от макисмальной цены к минимальной
-    url(r'^product/?select=min', views.product, name='product'), # Сортировка товаров от минимальной цены к максимальной
-    url(r'^contact/$', views.contact, name='contact'), # Пользователи
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    ## Products
+    # Parameters: ID, price, name
+    # Method: GET
+    url(r'^product/$', views.product, name='product'),
+    ## Sort of Products by max
+    # Parameters: max
+    # Method: POST
+    url(r'^product/?select=max', views.product, name='product'),
+    ## Sort of Products by min
+    # Parameters: min
+    # Method: POST
+    url(r'^product/?select=min', views.product, name='product'),
+    ## Users
+    # Parameters: name, email
+    # Method: GET
+    url(r'^contact/$', views.contact, name='contact'),
 ]
